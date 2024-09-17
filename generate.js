ec9f79fe824b2ea9f04e26b1162f11eea742dee8
Himangshuroy101 <geeksbuddy05@gmail.com>
2024-09-18 02:52:03 +0530

    update code file

--- generate.js
+++ generate.js
@@ -9,6 +9,14 @@ btn.addEventListener('click', function(){
 })
 
 function generateQR(){
+    if(qrText.value.trim().length > 0){
+
     qrImage.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+qrText.value;
     imageBox.classList.add("show-image");
+    }else{
+        qrText.classList.add('error');
+        setTimeout(()=>{
+            qrText.classList.remove('error');
+        }, 1000)
+    }
 }
